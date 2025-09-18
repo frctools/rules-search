9e006e4e2b7db99279db37096ae54684e9e93b2e
GrahamSH <grahamshllk@gmail.com>
2025-09-18 01:45:45 +0000

    add embed page

--- app.vue
+++ app.vue
@@ -1,15 +1,25 @@
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
-    <div class="flex flex-col min-h-screen">
-    <div class="flex-1">
-    <NuxtPage />
-   </div>
-    <div class="w-full flex justify-center gap-4 md:gap-8 items-center p-2 bg-gray-50/50 dark:bg-blue-950/50 md:flex-row flex-col ">
-      <span class="font-display italic">built by <a href="https://grahamsh.com" class="underline text-blue-700 font-semibold">grahamsh</a></span>
-      <BuyMeACoffee />
+    <div class="flex flex-col " :class="{'min-h-screen': !embed}">
+      <div class="flex-1">
+        <NuxtPage />
+      </div>
+      <div
+        class="w-full flex justify-center gap-4 md:gap-8 items-center p-2 bg-gray-50/50 dark:bg-blue-950/50 md:flex-row flex-col"
+        v-if="!embed"
+      >
+        <span class="font-display italic"
+          >built by
+          <a
+            href="https://grahamsh.com"
+            class="underline text-blue-700 font-semibold"
+            >grahamsh</a
+          ></span
+        >
+        <BuyMeACoffee />
+      </div>
     </div>
-   </div>
   </UApp>
 </template>
 <script setup>
@@ -17,7 +27,8 @@ onMounted(() => {
   if (
     window &&
     !localStorage.getItem("bingo-25-shown") &&
-    new Date().getTime() > 1735102800000 && false
+    new Date().getTime() > 1735102800000 &&
+    false
   ) {
     // christmas 2024
     const toast = useToast().add({
@@ -41,6 +52,10 @@ onMounted(() => {
     });
   }
 });
+const route = useRoute();
+const embed = computed(()=> {
+   return route.path.includes('embed');
+})
 useSeoMeta({
   titleTemplate: (x) => (x ? `${x} | FRCTools` : `FRCTools`),
   ogTitle: "FRCTools",
@@ -49,8 +64,7 @@ useSeoMeta({
 });
 useHead({
   htmlAttrs: {
-    lang: 'en',
-  }
+    lang: "en",
+  },
 });
-
 </script>
---
+++ pages/[year]/rule/[rule]/embed.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <UContainer class="flex flex-col gap-4 mt-4" v-if="!error">
+      <UBreadcrumb :items="items" />
+
+      <div
+        ref="container"
+        :class="{ 'max-h-100 overflow-hidden': !expanded }"
+        class="relative"
+      >
+        <RenderHtml :html="data.text" />
+        <div
+          v-if="isOverflowing"
+          class="bottom-0 w-full absolute bg-white/45 backdrop-blur-md p-2 cursor-pointer font-bold"
+          @click="expanded = !expanded"
+        >
+          {{expanded ? `Shrink Rule`:`Expand Rule`}} <UIcon :name="expanded ? `heroicons:arrow-up`: `heroicons:arrow-down`"></UIcon>
+        </div>
+      </div>
+    </UContainer>
+    <UContainer class="flex flex-col gap-4 mt-4" v-else>
+      <div
+        class="flex flex-col items-center justify-center flex-1 px-6 py-14 sm:px-14"
+      >
+        <UIcon
+          name="heroicons:exclamation-circle"
+          class="size-10 mx-auto text-gray-400 dark:text-gray-500 mb-4"
+        ></UIcon>
+
+        <p class="text-sm text-center text-gray-900 dark:text-white">
+          Something went wrong
+        </p>
+      </div>
+    </UContainer>
+  </div>
+</template>
+
+<script setup>
+import { upperFirst } from "scule";
+const route = useRoute();
+const validYears = useYears();
+if (!validYears.includes(route.params.year)) {
+  await navigateTo(`/${validYears[0]}`);
+}
+const year = ref(route.params.year);
+const expanded = ref(false);
+const el = useTemplateRef("container");
+const isOverflowing = ref(false);
+onMounted(() => {
+  isOverflowing.value = el.value?.scrollHeight > el.value?.clientHeight;
+});
+const rule = ref(route.params.rule);
+const { data, status, error, clear } = await useFetch("/api/rule", {
+  query: {
+    year: year.value,
+    query: rule.value,
+  },
+});
+const items = computed(() => {
+  return [
+    {
+      label: "Home",
+      to: "/",
+      icon: "i-lucide-house",
+      target: "_blank",
+    },
+    {
+      label: year.value,
+      to: `/${year.value}`,
+      icon: "i-lucide-calendar",
+      target: "_blank",
+    },
+    {
+      label: `${upperFirst(data.value.type)} ${data.value.name}`,
+      to: `/${year.value}/rule/${rule.value}`,
+    },
+  ];
+});
+useSeoMeta({
+  title: `${upperFirst(data.value.type)} ${data.value.name}`,
+  ogTitle: `${upperFirst(data.value.type)} ${data.value.name}`,
+  ogImage: data.value.additionalContent.find((x) => x?.type == "image")?.src,
+  description: data.value.summary,
+  ogDescription: data.value.summary,
+});
+useHead({
+  link: [
+    {
+      href: `https://frctools.com/api/apub?year=${year.value}&query=${rule.value}`,
+      rel: "alternate",
+      title: "ActivityPub (JSON)",
+      type: "application/activity+json",
+    },
+  ],
+});
+</script>
+
+<style></style>
