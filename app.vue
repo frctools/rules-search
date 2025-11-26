5fe856aa4b7cabb4b444764ede77537e75b8daeb
Graham <grahamshllk@gmail.com>
2025-11-25 22:29:28 -0500

    Update toast notification for FRCTools Orders

--- app.vue
+++ app.vue
@@ -23,29 +23,27 @@
   </UApp>
 </template>
 <script setup>
+  const route = useRoute();
 onMounted(() => {
   if (
     window &&
-    !localStorage.getItem("bingo-25-shown") &&
-    new Date().getTime() > 1735102800000 &&
-    false
+    !localStorage.getItem("orders-25-shown") && !route.fullPath.includes("embed")
   ) {
-    // christmas 2024
     const toast = useToast().add({
-      title: "Kickoff Bingo",
-      description: `Check out kickoff bingo!`,
-      icon: "i-lucide-calendar-days",
+      title: "Introducing FRCTools Orders",
+      description: `Plan, purchase, and receive parts with ease.`,
+      icon: "i-lucide-package",
       duration: 0,
       actions: [
         {
           icon: "i-lucide-square-arrow-out-up-right",
-          label: "Go to bingo",
+          label: "Open FRCTools Orders",
           color: "neutral",
           variant: "outline",
           onClick: async (e) => {
             e?.stopPropagation();
-            localStorage.setItem("bingo-25-shown", true);
-            await navigateTo("/bingo");
+            localStorage.setItem("orders-25-shown", true);
+            await navigateTo("https://orders.frctools.com", {external: true});
           },
         },
       ],
